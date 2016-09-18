4c155782e887cb92fadf6698df2efc653f3f791c
deg4uss3r <ricky.hosfelt@gmail.com>
2016-09-18 09:05:12 +0100

    added some more to the string to get a clearner line of text

--- plugins/rd_bot.py
+++ plugins/rd_bot.py
@@ -12,10 +12,19 @@ def get_weather(city):
     r = r_block.json()
     temp = r['main']['temp']
     wind = r['wind']['speed']
+    country = r['sys'['country']
+    city = r['id']['name']
 
-    response = " Current Temp: " + str(temp) + " wind speed: " + str(wind)
+    response = " Current weather for " + city + ", " + country + str(temp) + " wind speed: " + str(wind)
 
     return response
+
+
+def get_beers(pub):
+    clientSecretFile = open(os.path.expanduser('~/Dropbox/private/untapped-private-api'), 'r') #have to use os.path.expanduser because of ~
+    CLIENTSECRET=clientSecretFile.read()
+    CLIENTID='189BD8671F3124A796C4B9C78BB8FED66DA4C4C9'
+    
 
 def process_message(data):
     #print(data) 
